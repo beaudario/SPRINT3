1e60db13937ebb40e941f8bac325df987312e524
Mohammed Yasin <0310437@student.rocvantwente.nl>
2019-03-12 14:12:22 +0100

    Opdracht 6.1 van sprint 3 Mohammed

---
+++ opdracht_6.1_mohammed/opdracht_6.1_mohammed/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace opdracht_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Luizenmoeder());
+        }
+
+
+        public static string Luizenmoeder() { return "Hallo allemaal, wal leuk dat je er bent"; }
+
+
+    }
+}
