a58290141559237b78561e06e7879a6a294c868f
beaudario <30291512+beaudario@users.noreply.github.com>
2019-03-20 22:14:40 +0100

    6.7 Initial commit

---
+++ 6.7/6.7/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hoelang mag de Fibonnaci reeks zijn (geef aan in getallen): ");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+        }
+
+        private static int fibonnaci(int len)
+    }
+}
