af552e70d14a5da38d4f4dd53bcee98a62bb3858
beaudario <30291512+beaudario@users.noreply.github.com>
2019-03-21 17:23:25 +0100

    6.14 Initial commit

---
+++ 6.14/6.14/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static int AgeCalc(int date)
+        {
+
+        }    
+    }
+}
