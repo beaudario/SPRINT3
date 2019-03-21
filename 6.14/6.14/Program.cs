535778ed4fffca99a0a0445bd564df2599fd9fea
beaudario <30291512+beaudario@users.noreply.github.com>
2019-03-21 22:54:51 +0100

    6.14 Done

--- 6.14/6.14/Program.cs
+++ 6.14/6.14/Program.cs
@@ -10,12 +10,20 @@ namespace _6._14
     {
         static void Main(string[] args)
         {
+            Console.Write("Geef geboortedatum (bijv. 03-07-1989): ");
+            string birthday = Console.ReadLine();
+            int birthday1 = int.Parse(birthday.Substring(6));
 
+            Console.WriteLine("De leeftijd is: {0}", AgeCalc(birthday1));
+            Console.ReadKey();
         }
 
-        private static int AgeCalc(int date)
+        private static int AgeCalc(int bday)
         {
+            DateTime now = DateTime.Today;
+            int age = now.Year - bday;
 
+            return age;
         }    
     }
 }
