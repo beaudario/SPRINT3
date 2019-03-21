7e0405b7ea21098dc992eb30c5300cb91c86d0a6
beaudario <30291512+beaudario@users.noreply.github.com>
2019-03-21 12:43:52 +0100

    6.10 Initial commit

---
+++ 6.10/6.10/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef eerste getal: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Geef tweede getal: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Swap(ref firstNumber, ref secondNumber);
+            Console.WriteLine("Het eerste getal is nu {0}", firstNumber);
+            Console.WriteLine("Het tweede getal is nu {0}", secondNumber);
+            Console.ReadKey();
+        }
+
+        static void Swap(ref int firstNum, ref int secondNum)
+        {
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = firstNum;
+        }
+    }
+}
