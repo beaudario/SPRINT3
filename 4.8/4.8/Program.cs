60648edce8843b661bb3b062b9946c0e94d8b579
Rotjochie666 <44999395+RenzoPX@users.noreply.github.com>
2019-03-21 14:01:11 +0100

    Opdracht 7.1

---
+++ 4.8/4.8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine("Hey, geef een getal in en krijg de maand in het de taal corresponderend met het getal.");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine("1. Nederlands");
+            1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("2. Duits");
+            Console.WriteLine("3. Engels");
+            Console.WriteLine("4. Frans");
+            Console.WriteLine("5. Spaans");
+            Console.WriteLine("6. Italiaans");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine("Geef hieronder een getal in.");
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.ReadLine();
+            string Nummer;
+            Nummer = Console.ReadLine();
+            DateTime maand = DateTime.Now();
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+            Console.WriteLine("Jouwn keuze is: " + Maand);
+            Console.WriteLine("--------------------------------------------------------------------------------------");
+
+            //Eerst tijd naar nu zetten, daarna userinput laten herkennen qua getallen. Daarna If statement voor 6 branches.//
+            
+            
+        }
+    }
+}
---
+++ Opdracht6.4/Opdracht6.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
