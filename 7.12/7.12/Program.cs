ac2da32d233bc5a19b8a1d40df032ef6b099815d
beaudario <30291512+beaudario@users.noreply.github.com>
2019-03-23 11:55:35 +0100

    7.12 Kei mooie opdracht maar hij is klaar!!!

--- 7.12/7.12/Program.cs
+++ 7.12/7.12/Program.cs
@@ -10,35 +10,88 @@ namespace _7._12
     {
         static void Main(string[] args)
         {
-            Console.Write("Geef een woord: ");
-            string Input = Console.ReadLine();
-            string Input1 = Input.ToUpper();
+            Choice();
+        }
+
+        public static void Choice()
+        {
+            Console.WriteLine("\n ********************************** \n");
+            Console.Write("[V]ersleutelen of [O]ntsleutelen: ");
+            string VO = Console.ReadLine();
+
+            if (VO == "V" || VO == "v")
+            {
+                Console.WriteLine("\n ********************************** \n");
+                Encrypter();
+            }
+
+            else if (VO == "O" || VO == "o")
+            {
+                Console.WriteLine("\n ********************************** \n");
+                Decrypter();
+            }
+        }
+
+        public static void Encrypter()
+        {
+            Console.Write("Geef een woord om te versleutelen: ");
+            string InputEn = Console.ReadLine();
             Console.WriteLine();
 
             Console.Write("Geef een key: ");
-            int Key = int.Parse(Console.ReadLine());
+            int KeyEn = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
-            Console.Write("Het ge-encrypte woord is: ");
-            Console.Write(Encrypter(Input1, Key));
+            Console.Write("Het versleutelde woord is: ");
+            Console.WriteLine(Encrypter1(InputEn, KeyEn));
             Console.ReadKey();
+            Choice();
         }
 
-        private static string Encrypter(string Input2, int Shift)
+        public static string Encrypter1(string Input, int Key)
         {
             string Output = null;
-            char[] Letters;
-            Letters = Input2.ToCharArray();
+            char[] Letters = Input.ToCharArray(); ;
             int Temp;
 
-            for (int i = 0; i < Input2.Length; i++)
+            for (int i = 0; i < Input.Length; i++)
             {
-                Temp = (int)(Letters[i] + Shift);
+                Temp = (int)(Letters[i] + Key);
                 Output += (char)Temp;
             }
 
             return Output;
         }
 
+        public static void Decrypter()
+        {
+            Console.Write("Geef een woord om te ontsleutelen: ");
+            string InputDe = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Geef een key: ");
+            int KeyDe = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Het ontsleutelde woord is: ");
+            Console.WriteLine(Decrypter1(InputDe, KeyDe));
+            Console.ReadKey();
+            Choice();
+        }
+
+        public static string Decrypter1(string Input, int Key)
+        {
+            string Output = null;
+            char[] Letters = Input.ToCharArray();
+            int Temp;
+
+            for (int i = 0; i < Input.Length; i++)
+            {
+                Temp = (int)(Letters[i] - Key);
+                Output += (char)Temp;
+            }
+
+            return Output;
+        }
     }
 }
