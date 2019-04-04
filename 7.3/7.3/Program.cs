fd0c6f83959172a87cecc4f54c8df081c0bf95f5
Jesse14865 <jesseradema4@gmail.com>
2019-04-04 16:35:23 +0200

    7.3 Done

---
+++ 7.3/7.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _7._3Jesse
+{
+    class Program
+    {
+        public static int[] randomnummers = new int[25];
+        static Random random = new Random();
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("hier ziet u de even getallen");
+            Console.ReadKey();
+            for (int i = 0; i < randomnummers.Length; i++)
+            {
+
+                int num = random.Next(1, 10000);
+                randomnummers[i] = num;
+            }
+            for (int i = 0; i < randomnummers.Length; i++)
+            {
+                if (randomnummers[i] % 2 == 0)
+                {
+                    Console.WriteLine(randomnummers[i]);
+                }
+            }
+            Console.WriteLine("hier ziet u de oneven getallen");
+            Console.ReadKey();
+            for (int i = 0; i < randomnummers.Length; i++)
+            {
+                if (randomnummers[i] % 2 != 0)
+                {
+                    Console.WriteLine(randomnummers[i]);
+                }
+
+            }
+            System.Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+
