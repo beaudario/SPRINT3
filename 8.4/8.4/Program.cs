8d51e432e4fff8ba9acacc3f04af49411ab8d2cc
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-04 16:35:58 +0200

    8.4 Done

--- 8.4/8.4/Program.cs
+++ 8.4/8.4/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Net.NetworkInformation;
+
 
 namespace _8._4
 {
@@ -11,7 +14,22 @@ namespace _8._4
     {
         static void Main(string[] args)
         {
-            
+            IPAddress ip;
+            Console.Write("Geef IP-Adres: ");
+            string IpAddress = Console.ReadLine();
+
+            bool IpCheck = IPAddress.TryParse(IpAddress, out ip);
+
+            if (IpCheck)
+            {
+                Console.WriteLine("Dit adres bereikbaar");
+            }
+            else
+            {
+                Console.WriteLine("Dit adres is niet bereikbaar");
+            }
+
+            Console.ReadKey();
         }
     }
 }
