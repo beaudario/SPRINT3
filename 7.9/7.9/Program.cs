f0e2fb6a93050e31cf985dc6396f7806343de57a
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-04 13:53:42 +0200

    7.9 Done

--- 7.9/7.9/Program.cs
+++ 7.9/7.9/Program.cs
@@ -10,7 +10,19 @@ namespace _7._9
     {
         static void Main(string[] args)
         {
-            
+            string[,] Info =
+            {
+                {"Dario Douma", "0311264", "25 November 2001", "Enschede"},
+                {"Jesse Radema", "0310146", "26 Mei 2001", "Enschede"},
+                {"Renzo Pieters", "a", "b", "Enschede"},
+                {"Mohammed Yasin", "c", "d", "Enschede" }
+            };
+
+            for (int i = 0; i < (Info.Length / 4); i++)
+            {
+                Console.WriteLine("{0} ({1}) is geboren op {2} in {3}", Info[i, 0], Info[i, 1], Info[i, 2], Info[i, 3]);
+            }
+            Console.ReadKey();
         }
     }
 }
