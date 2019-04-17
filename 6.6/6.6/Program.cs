ced485debf33da9765a06fee84e3923c438449eb
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:02:00 +0200

    6.6 Comments

--- 6.6/6.6/Program.cs
+++ 6.6/6.6/Program.cs
@@ -10,24 +10,31 @@ namespace _6._6
     {
         static void Main(string[] args)
         {
+            //UserInput
             Console.Write("Geef eerste nummer: ");
             var baseNumber = Convert.ToInt32(Console.ReadLine());
             Console.Write("Geef tweede nummer: ");
             var exponent = Convert.ToInt32(Console.ReadLine());
 
+            //SendParameterToMethod
             var sum = IntegerPower(baseNumber, exponent);
+
+            //ShowResult
             Console.WriteLine("{0} tot de macht {1} is {2}", baseNumber, exponent, sum);
             Console.ReadLine();
         }
-
+        
+        //Method
         private static int IntegerPower(int baseNum, int expo)
         {
+            //Calculating
             var result = 1;
             for (var i = 0; i < expo; i++)
             {
                 result *= baseNum;
             }
 
+            //ReturnResult
             return result;
         }
 
