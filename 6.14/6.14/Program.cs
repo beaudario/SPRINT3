2ad9a21d884fb1060e1f9a6b311549740e06890c
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 19:59:56 +0200

    6.14 Comments

--- 6.14/6.14/Program.cs
+++ 6.14/6.14/Program.cs
@@ -10,19 +10,26 @@ namespace _6._14
     {
         static void Main(string[] args)
         {
+            //UserInput
             Console.Write("Geef geboortedatum (bijv. 03-07-1989): ");
             string birthday = Console.ReadLine();
+
+            //DeclaringVariable
             int birthday1 = int.Parse(birthday.Substring(6));
 
+            //ShowResult
             Console.WriteLine("De leeftijd is: {0}", AgeCalc(birthday1));
             Console.ReadKey();
         }
 
+        //Method
         private static int AgeCalc(int bday)
         {
+            //CalculatingAge
             DateTime now = DateTime.Today;
             int age = now.Year - bday;
 
+            //ReturnResult
             return age;
         }    
     }
