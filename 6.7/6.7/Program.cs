18cc0ac43de549b3877af39dc635e0c619c8f28a
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:04:20 +0200

    6.7 Comments

--- 6.7/6.7/Program.cs
+++ 6.7/6.7/Program.cs
@@ -10,29 +10,37 @@ namespace _6._7
     {
         static void Main(string[] args)
         {
+            //UserInput
             Console.Write("Hoeveel getallen mag de reeks bevatten? (geef aan in getallen): ");
             int lenght = Convert.ToInt32(Console.ReadLine());
 
+            //ForLoop
             for (int i = 0; i < lenght; i++)
             {
+                //ShowResult
                 Console.Write("{0} ", Fibonacci(i));
             }
 
             Console.ReadKey();
         }
 
+        //Method
         public static int Fibonacci(int len)
         {
+            //DeclaringVariables
             int firstNum = 0;
             int secondNum = 1;
             
+            //ForLoop
             for (int i = 0; i < len; i++)
             {
+                //CalculatingFibonacci
                 int sum = firstNum;
                 firstNum = secondNum;
                 secondNum = sum + secondNum;
             }
 
+            //ReturnResult
             return firstNum;
         }
     }
