d44b8ebdc7ad26e2ff60b6e5a011c6d876adabe9
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:21:24 +0200

    8.3 Comments

--- 8.3/8.3/Program.cs
+++ 8.3/8.3/Program.cs
@@ -10,6 +10,7 @@ namespace _8._3
     {
         static void Main(string[] args)
         {
+            //UserInput
             Console.Write("Geef deelgetal: ");
             double Number = double.Parse(Console.ReadLine());
             Console.WriteLine();
@@ -18,13 +19,17 @@ namespace _8._3
             double DivideNumber = double.Parse(Console.ReadLine());
             Console.WriteLine();
 
+            //IfStatement
             if (DivideNumber == 0)
             {
                 Console.WriteLine("Wie deelt door nul is een snul!");
             }
             else
             {
+                //Calculating
                 double DividedNumber = Number / DivideNumber;
+
+                //ShowResult
                 Console.WriteLine("{0} / {1} = {2}", Number, DivideNumber, DividedNumber);
             }            
             
