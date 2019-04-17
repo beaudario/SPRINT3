1074e86bae97c58b6d1c83679b01bcb7bfb93d93
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:14:32 +0200

    7.12 Comments

--- 7.12/7.12/Program.cs
+++ 7.12/7.12/Program.cs
@@ -10,23 +10,29 @@ namespace _7._12
     {
         static void Main(string[] args)
         {
+            //CallingMethod
             Choice();
         }
 
+        //Method
         public static void Choice()
         {
+            //UserInput
             Console.Write("\n ********************************** \n");
             Console.Write("[V]ersleutelen of [O]ntsleutelen: ");
             string VO = Console.ReadLine();
 
+            //IfStatement
             if (VO == "V" || VO == "v")
             {
+                //CallingMethod
                 Console.WriteLine("\n ********************************** \n");
                 Encrypter();
             }
 
             else if (VO == "O" || VO == "o")
             {
+                //CallingMethod
                 Console.WriteLine("\n ********************************** \n");
                 Decrypter();
             }
@@ -34,15 +40,16 @@ namespace _7._12
 
         public static void Encrypter()
         {
+            //UserInput
             Console.Write("Geef een woord om te versleutelen: ");
             string InputEn = Console.ReadLine();
-            
             Console.WriteLine();
 
             Console.Write("Geef een key: ");
             int KeyEn = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
+            //ShowResult
             Console.Write("Het versleutelde woord is: {0}", (Encrypter1(InputEn, KeyEn)));
             Console.WriteLine();
             Console.ReadKey();
@@ -50,23 +57,30 @@ namespace _7._12
             Choice();
         }
 
+        //Method
         public static string Encrypter1(string Input, int Key)
         {
+            //DeclaringVariablesAndArray
             string Output = null;
             char[] Letters = Input.ToCharArray(); ;
             int Temp;
 
+            //ForLoop
             for (int i = 0; i < Input.Length; i++)
             {
+                //Shift
                 Temp = (int)(Letters[i] + Key);
                 Output += (char)Temp;
             }
 
+            //ReturnResult
             return Output;
         }
 
+        //Method
         public static void Decrypter()
         {
+            //UserInput
             Console.Write("Geef een woord om te ontsleutelen: ");
             string InputDe = Console.ReadLine();
             Console.WriteLine();
@@ -75,25 +89,32 @@ namespace _7._12
             int KeyDe = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
+            //ShowResult
             Console.Write("Het ontsleutelde woord is: {0}", (Decrypter1(InputDe, KeyDe)));
             Console.WriteLine();
             Console.ReadKey();
 
+            //CallingMethod
             Choice();
         }
 
+        //Method
         public static string Decrypter1(string Input, int Key)
         {
+            //DeclaringVariablesAndArray
             string Output = null;
             char[] Letters = Input.ToCharArray();
             int Temp;
 
+            //ForLoop
             for (int i = 0; i < Input.Length; i++)
             {
+                //Unshift
                 Temp = (int)(Letters[i] - Key);
                 Output += (char)Temp;
             }
 
+            //ReturnResult
             return Output;
         }
     }
