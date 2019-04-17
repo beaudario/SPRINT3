a0539a2e0278bf410540e2c4af4e198fc38a3a09
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:30:29 +0200

    7.2 Comments

--- Opdracht7.2/Opdracht7.2/Program.cs
+++ Opdracht7.2/Opdracht7.2/Program.cs
@@ -11,10 +11,15 @@ namespace Opdracht7._2
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo wij geven een random naam van ons groepje weer.");
+
+            //DeclaringArray
             string[] random = new string[4] { "Renzo", "Dario", "Jesse", "Mohammed"};
 
+            //DeclaringNewRandom
             Random rand = new Random();
             int index = rand.Next(random.Length);
+
+            //ShowResult
             Console.WriteLine($"Dit is het random geselecteerde groepslid: {random[index]}");
             Console.ReadKey();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ;
 
