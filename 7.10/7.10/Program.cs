19bceb230bd00010ff2523ffb642e7b7597b0f1f
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:08:56 +0200

    7.10 Comments

--- 7.10/7.10/Program.cs
+++ 7.10/7.10/Program.cs
@@ -10,13 +10,14 @@ namespace _7._10
     {
         static void Main(string[] args)
         {
+            //DeclaringArrays
             string[] Name = {
                 "Dario Douma",
                 "Jesse Radema",
                 "Renzo Pieters",
                 "Mohammed Yasin"
             };
-
+            
             double[,] Grades = {
                 {5.4, 7.0, 5.6},
                 {5.5, 4.9, 5.7},
@@ -24,24 +25,28 @@ namespace _7._10
                 {1.0, 3.5, 5.5}
             };
 
+            //ForLoop
             for (int i = 0; i < (Name.Length); i++)
             {
+                //DeclaringVariable
                 double Sum = 0;
 
+                //ForLoop
                 for (int i1 = 0; i1 < 3; i1++)
                 {
+                    //Calculating
                     Sum += Grades[i, i1];
                 }
 
                 Sum += Grades[i, 2];
                 double Average = Sum / 4;
 
+                //ShowResult
                 Console.WriteLine("{0}: \n", Name[i]);
                 Console.WriteLine("Nederlands (weging 1): {0}", Grades[i, 0]);
                 Console.WriteLine("Engels (weging 1): {0}", Grades[i, 1]);
                 Console.WriteLine("Nederlands (weging 2): {0}", Grades[i, 2]);
                 Console.WriteLine("Gemiddelde: {0}", Average);
-
                 Console.WriteLine("*************************************** \n");
             }
 
