9d59586409b082ca525610659ac09533f629ce85
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:25:54 +0200

    8.5 Comments

--- 8.5/8.5/Program.cs
+++ 8.5/8.5/Program.cs
@@ -1,37 +1,33 @@
 using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
-
-
 
 namespace ConsoleApp1
 
 {
-
     class Program
-
     {
+        //DeclaringNewRandom
         static Random r = new Random();
 
         static void Main(string[] args)
 
         {
+            //ForLoop
             for (int i = 0; i < 100; i++)
             {
-
+                //GeneratingRandomNumber
                 long getal1 = r.Next(int.MaxValue);
                 long getal2 = r.Next(int.MaxValue);
+
+                //Calculating
                 long product = getal1 * getal2;
 
+                //ShowResult
                 Console.WriteLine("{0} * {1} = {2}", getal1, getal2, product);
             }
-
             Console.ReadKey();
         }
     }
