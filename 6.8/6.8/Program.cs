3bb3614bee85e6ccb41f8df4de9baad59c39d452
beaudario <30291512+beaudario@users.noreply.github.com>
2019-04-17 20:06:26 +0200

    6.8 Comments

--- 6.8/6.8/Program.cs
+++ 6.8/6.8/Program.cs
@@ -10,21 +10,30 @@ namespace _6._8
     {
         static void Main(string[] args)
         {
+            //UserInput
             Console.Write("Geef een nummer: ");
             int number = Convert.ToInt32(Console.ReadLine());
 
+            //ShowResult
             Console.WriteLine("De som van de individuele getallen van het nummer {0} is {1}", number, SumCal(number));
             Console.ReadKey();
         }
 
+        //Method
         private static int SumCal(int num)
         {
+            //DeclaringVariables
             string num1 = Convert.ToString(num);
             int sum = 0;
+
+            //ForLoop
             for (int i = 0; i < num1.Length; i++)
             {
+                //Calculating
                 sum += Convert.ToInt32(num1.Substring(i, 1));
             }
+
+            //ReturnResult
             return sum;
         }
     }
